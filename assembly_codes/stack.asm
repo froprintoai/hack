2a706b93188d36bb7fd28f8e3460a7c668815669
froprintoai <froprintoai@gmail.com>
2020-11-11 09:45:42 -0600

    stack added

---
+++ assembly_codes/stack.asm
@@ -0,0 +1,35 @@
+section .data
+
+EXIT_SUCCESS equ 0
+SYS_exit equ 60
+
+numbers dq 121, 122, 123, 124, 125
+len dq 5
+
+section .text
+global _start
+_start:
+
+mov rcx, qword [len]
+mov rbx, numbers
+mov r12, 0
+mov rax, 0
+
+pushLoop:
+    push qword [rbx+r12*8]
+    inc r12
+    loop pushLoop
+
+mov rcx, qword [len]
+mov rbx, numbers
+mov r12, 0
+popLoop:
+    pop rax
+    mov qword [rbx+r12*8], rax
+    inc r12
+    loop popLoop
+
+last:
+    mov rax, SYS_exit
+    mov rdi, EXIT_SUCCESS
+    syscall
